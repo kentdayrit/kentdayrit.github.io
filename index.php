30a6f28d9d3660a9e25bcc8673b158b13b683553
Kent Dayrit <kdpogz19@gmail.com>
2020-04-10 22:01:08 +0800

    add metadata in html

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
 	<head>
 		<title>Kent Dayrit</title>
-		<!-- <link rel="stylesheet" type="text/css" href="assets/css/app.css"> -->
-		<!-- <script src="assets/js/app.js"></script> -->
+			<meta charset="utf-8">
+		 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- <link rel="stylesheet" type="text/css" href="assets/css/app.css">
+		<script src="assets/js/app.js"></script> -->
 		
 		<link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
