3ce9e878ebab28b78168d55f10dd5601b90b30bb
Kent Dayrit <kdpogz19@gmail.com>
2020-04-24 19:32:02 +0800

    edit booble description

--- app/pages/sections/projects-section.php
+++ app/pages/sections/projects-section.php
@@ -8,58 +8,56 @@
 	</div>
 	<div class="row p-3">
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="http://lvcc-online-admission-system.herokuapp.com/login" class="text-dark">LVCC ONLINE ADMISSION SYSTEM</a></h3>
+			<h3><a href="http://lvcc-online-admission-system.herokuapp.com/login" target="_blank" class="text-dark">LVCC ONLINE ADMISSION SYSTEM</a></h3>
 			<span><small><b><i>SCHOOL MANAGEMENT SYSTEM</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2">
 				This system was dedicated for La Verdad Christian College for fast and effective online admission process of the aspiring students. It minimizes the student's burden going to the school when they are apply for the school scholarship. The system will send the credentials of the student including the requirements for the scholarship to the staffs of the school and the system will also notify the student applicant for there applicantion. The system covers the whole admission process and enrollment system api ready.
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="" class="text-dark">GcTraders.com.au</a></h3>
+			<h3><a href="" target="_blank" class="text-dark">GcTraders.com.au</a></h3>
 			<span><small><b><i>E-COMMERCE WEBSITE</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2">
 				GCTrader is a hybrid ecommerce website the system fetures the typical buy and sell functionalities and auction fuctionality. This system was developed using laravel 6 for backend and javascrip, jquery, css, bootstrap and tailwind for frontend. In addition the the system use AWS for the server and for file storage.
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="" class="text-dark">ERPS FOR LUCKY FABS</a></h3>
+			<h3><a href="" target="_blank" class="text-dark">ERPS FOR LUCKY FABS</a></h3>
 			<span><small><b><i>ENTERPRISE RESOURCE PLANNING SYSTEM</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
 				Lucky Fubs is a Enterprise Resource Planing Management System Intended for Lucky FubsClothes Manufacturing Company. This will monitor the Clothes making process, Employement, Inventory, Payrol and etc. This system developed using Codeignater for backend, Nodejs and Json for API, and Bootstrap, Javascript, JQuery and Ajax for frontend
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="" class="text-dark">BOOBLE.COM.AU</a></h3>
+			<h3><a href="http://booble.com.au/" target="_blank" class="text-dark">BOOBLE.COM.AU</a></h3>
 			<span><small><b><i>E-COMMERCE WEBSITE</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				Booble is an e-commerce website indended for a business located in Australlia. The system featuring the Login and Register function with an online booking for the services you need and also an online payment. The website has an admin dashbord for sales, transaction and business statistics monitoring. The system developed using laravel for backend and js jquery bootstrap for frondend development.
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="" class="text-dark">INVENTORY SYSTEM POS</a></h3>
+			<h3><a href="" target="_blank" class="text-dark">INVENTORY SYSTEM POS</a></h3>
 			<span><small><b><i>WINDOWS APPLICATION</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
 				ISPOS is a simple Windows Application for Grocery System. The system manage the Inventory and Sales of the Store. This System developed using javascript Electron Framework including Angular 6 and Bootsrap
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="https://inventory-system-marcide-blue.000webhostapp.com/index.php" class="text-dark">ONLINE INVENTORY SYSTEM </a></h3>
+			<h3><a href="https://inventory-system-marcide-blue.000webhostapp.com/index.php" target="_blank" class="text-dark">ONLINE INVENTORY SYSTEM </a></h3>
 			<span><small><b><i>WEBSITE APPLICATION</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
 				Online Inventory System is tool intended for Marcide Blue Water Company. It monitor the item inventory and Sales in every branches of Company. The system developed using Laravel 5 php framework for backend and Bootsrap for frontend.
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="http://lvcc-library-system.herokuapp.com/" class="text-dark">LVCC LIBRARY SYSTEM POS</a></h3>
+			<h3><a href="http://lvcc-library-system.herokuapp.com/" target="_blank" class="text-dark">LVCC LIBRARY SYSTEM POS</a></h3>
 			<span><small><b><i>WINDOWS APPLICATION</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
 				LVCC Library System is a software dedicated to La Verdad Christian College. This tool will manage whole library process including the books data management, borrowing and buying books management and books location management. This system is developed using Laravel 5 php framework for backend and bootrap Javascript JQuery and Ajax for fronend.
 			</p>
 		</div>
 		<div class="col-md-5 text-center mx-auto mt-5 p-3 card card-body" style="background-color: #dae3f3;">
-			<h3><a href="http://lvcc-library-system.herokuapp.com/" class="text-dark">LA VERDAD SUITE</a></h3>
+			<h3><a href="http://lvcc-library-system.herokuapp.com/" target="_blank" class="text-dark">LA VERDAD SUITE</a></h3>
 			<span><small><b><i>WEBSITE APPLICATION</i></b></small></span>
 			<p class="ml-2 mr-2 mt-2 ">
 				LVCC Suite which is also known as the La Verdad Christian College Suite is a tool dedicated and intended for every La Verdad Christian College's students and staffs. It is designed to support the educational institution's needs in promoting academic excellence. It showcases an adaptive learning methodology contained in a single system called Adaptive Learning System (ALS). Also it will serve as an efficient portal mainly for students which encourage them to try and do advance studying in different learning areas. This will enhance their potentials and making them more prepared towards the next years of academic studies. This system developed using Laravel 5 php framework for backend and Semantic UI for fronend.
