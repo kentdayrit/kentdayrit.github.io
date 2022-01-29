37bbe7e4df2b1d16f82ff7c3f485eefd6bb50894
vagrant <vagrant@homestead>
2022-01-29 05:43:56 +0000

    add job log

--- app/pages/sections/experiences-section.php
+++ app/pages/sections/experiences-section.php
@@ -1,5 +1,5 @@
 <!-- EXPERIENCES -->
-<section class="container mt-5 pt-4 pb-5" id="experiences-section" style="background-color: #dae3f3;">
+<section class="container mt-5 pt-4 pb-5" id="experiences-section" >
 	<div class="row">
 		<div class="col-md-11 mx-auto text-center">
 			<h1 style="color: #002060; font-family: Century gothic"><i class="fas fa-cogs display-5"></i> EXPERIENCES</h1>
@@ -7,20 +7,20 @@
 		</div> 	
 	</div>
 	<div class="row col-md-12 mx-auto">
-		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #222a35;">
+		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #2a6599;">
 			<i class="fas fa-user-tie display-4 mb-3"></i>
 			<h2 style="font-family: Arial">FREELANCER</h2>
 			<h5>SOFTWARE DEVELOPER <b>|</b> WEBSITE DEVELOPER</h5>
 			<span><i>MAY 2019 - APRIL 2020</i></span>
 		</div> 
-		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #222a35;">
-			<i class="fas fa-user-tie display-4 mb-3"></i>
+		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #2a6599;">
+			<img class="img-fluid mb-4" width="100" height="auto" src="/assets/img/webx-purple.png">
 			<h2 style="font-family: Arial">WEBX ASIA</h2>
 			<h5>SOFTWARE DEVELOPER</h5>
 			<span><i>JUNE - NOVEMBER 2019</i></span>
 		</div>
-		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #222a35;">
-			<i class="fas fa-briefcase display-4 mb-3"></i>
+		<div class="col-md-5 mt-5 p-3 mx-auto text-center border rounded-lg" style="background-color: #b4c7e7; color: #2a6599;">
+		<img class="img-fluid mb-4" width="100" height="auto" src="/assets/img/hivelabs_icon.png">
 			<h2 style="font-family: Arial">HIVE LABS TECHNOLOGY</h2>
 			<h5>WEB DEVELOPER | MENTOR | PRODUCT LEAD</h5>
 			<span><i>JULY 2020 - CURRENT</i></span>
